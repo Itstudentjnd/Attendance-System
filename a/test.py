da2f638f6796733953ac164b178803bd8c5fa9d9
Itstudentjnd <87485552+Itstudentjnd@users.noreply.github.com>
2024-07-11 15:08:43 +0530

    Delete a directory

--- a/test.py
+++
@@ -1,5 +0,0 @@
-import whatsapp_auto as kit
-import what_gui as wg
-
-#kit.sendwhatmsg_instantly("+916354112472", "Your child is Absent")
-wg.send_whatsapp_message("+916354112472", "Your child is Absent")
